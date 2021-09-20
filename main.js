268b23c9833248cbe489ef57bab7dee6a5920aed
Leon <hualiang.tian@gmail.com>
2021-09-19 22:14:01 -0400

    commit tree 8e7a7a5814626bdd7d0808df748deba4396bc7f3

--- main.js
+++ main.js
@@ -1,8 +1,69 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
+const hearts= document.getElementsByClassName("like-glyph");
 
 // Your JavaScript code goes here!
+
+// document.addEventListener("DOMContentLoaded", () => 
+// {
+//   for (const heart of hearts)
+//   {
+//     heart.addEventListener("click", likeClicker)
+//   }
+// }); 
+
+// function likeClicker(e)
+// {
+//   let like = e.target;
+//     if (like.innerHTML == EMPTY_HEART)
+//     {
+//       mimicServerCall()
+//       .then(() => 
+//       {
+//        like.innerHTML = FULL_HEART;
+//        like.setAttribute("class", "liked");
+//       })
+//       .catch(error => 
+//       {
+//         const modal = document.getElementById("modal");
+//         modal.removeAttribute("class","hidden");
+//         modal.innerText = "You are now seeing an error Message =]";
+//         setTimeout(() => modal.setAttribute("class","hidden"),6000);
+//       })
+//     }
+//     else 
+//     {
+//       like.innerHTML = EMPTY_HEART;
+//       like.removeAttribute("class","like");
+//     }    
+// };
+
+modal.className = "hidden"
+
+for (const heart of hearts)
+{
+  heart.addEventListener("click", likeClicker)
+}
+
+function likeClicker()
+{
+  mimicServerCall()
+ .then(() => 
+  {
+   heart.className = "liked"
+  })
+  .catch(() => 
+  {
+    modal.style.visibility = "visible"
+  })
+};
+
+document.addEventListener("DOMContentLoaded", () => 
+{
+
+}); 
+
 
 
 
